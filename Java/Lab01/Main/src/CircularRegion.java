f7c591ee78510622e5b1e371e0a841964ec8bb61
u-sri2002 <usrimart@gmail.com>
2024-08-06 22:27:10 +0530

    complete lab1 on OOP

---
+++ Java/Lab01/Main/src/CircularRegion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CircularRegion {
+
+    public static final float PI = 3.14159f;
+    public static void main(String[] args) {
+        
+        Scanner uScanner = new Scanner(System.in);
+        System.out.println("Enter the inner radius: ");
+        float R_in = uScanner.nextFloat();
+        
+        System.out.println("Enter the outer radius: ");
+        float R_out = uScanner.nextFloat();
+
+        double innerArea = PI * Math.pow(R_in, 2);
+        double outerArea = PI * Math.pow(R_out, 2);
+
+        double shadedArea = outerArea - innerArea;
+
+        System.out.println("The are of shaded area is : "+ Math.round(shadedArea));
+
+    }
+}
