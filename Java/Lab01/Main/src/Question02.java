04b95a909c4ae87fd92029b1db359b23a5c5cdd7
Udasri Hasindu <udasrihasindu@localhost.localdomain>
2024-08-17 09:09:00 +0530

    done question 2

---
+++ Java/Lab01/Main/src/Question02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Question02 {
+
+    public static void main(String[] args){
+
+        // Scanner object
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String firstName = userInput.nextLine();
+
+        System.out.println("Enter your middle name: ");
+        String middleName = userInput.nextLine();
+
+        System.out.println("Enter your last name: ");
+        String lastName = userInput.nextLine();
+
+        // defining name format
+        String businessName = lastName + ", " + firstName + " " + middleName.charAt(0) + ".";
+        System.out.println("Your business name is: \n" + businessName );
+
+    }
+}
