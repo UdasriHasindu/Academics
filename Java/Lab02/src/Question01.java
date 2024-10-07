b89fa042852c9cc87cbbbe53e082cdc2e403f2f5
u-sri2002 <usrimart@gmail.com>
2024-10-07 06:43:11 +0530

    Finish question01

---
+++ Java/Lab02/src/Question01.java
@@ -0,0 +1,16 @@
+
+public class Question01 {
+    public static void main(String[] arg) {
+
+        int j=0;
+        for(int i = 10; i < 50; i++){
+            ++j;
+            if (j > 10){
+               System.out.println();
+               j =1;
+
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
