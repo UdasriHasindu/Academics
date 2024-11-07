ed7fbe7cf9c0ffd2a91afe3f9a544b9f75a3969f
u-sri2002 <usrimart@gmail.com>
2024-11-07 12:32:17 +0530

    finish q5

--- Java Labs/Lab05/src/Main.java
+++ Java Labs/Lab05/src/Main.java
@@ -1,6 +1,7 @@
 import Question02.library.books.FictionBooks;
-import Question03.BankAccount;
 import static Question02.library.utils.BookUtils.printBookDetails;
+import Question03.BankAccount;
+import Question05.AreaCalculator;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +20,14 @@ public class Main {
 
         acc.withdraw(12000.00); // withdraw mone from account
         System.out.println("Current bank balance is : " + acc.getBalance());
+
+        // Question 05
+        int rectangleLength = 5;
+        int rectangleWidth = 8;
+        int squareSideLength = 4;
+
+        System.out.println("Area of the rectangle = " + AreaCalculator.calculateRectangleArea(rectangleLength, rectangleWidth));
+        System.out.println("Area of the square = " + AreaCalculator.calculateSquareArea(squareSideLength));
 
 
     }
---
+++ Java Labs/Lab05/src/Question05/AreaCalculator.java
@@ -0,0 +1,12 @@
+package Question05;
+
+public class AreaCalculator {
+
+    public static int calculateRectangleArea(int length, int width){
+        return length * width;
+    }
+
+    public static int calculateSquareArea(int sideLength){
+        return sideLength * sideLength;
+    }
+}
