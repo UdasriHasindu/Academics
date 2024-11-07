5d9752703bb033e6d1970becc3dac60329de5f06
u-sri2002 <usrimart@gmail.com>
2024-11-07 20:02:29 +0530

    change gitignore file

--- DSA/Labs/test.c
+++ DSA/Labs/test.c
@@ -4,9 +4,8 @@ void main(){
 
     int arr[5] = {1, 4, 3, 8, 5};
 
-    printf("%d \n", sizeof(arr));
-    // printf("%d \n", arr[2]);
-    // arr[2] = 15;
-    // printf("%d \n", arr[2]);
+    printf("%d \n", arr[2]);
+    arr[2] = 15;
+    printf("%d \n", arr[2]);
 
 }
